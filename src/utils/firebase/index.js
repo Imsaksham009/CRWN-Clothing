ba8a49d7c8f4f8b84b1307962a347b0d4ca920ac
Saksham Gupta <guptasaksham82@gmail.com>
2023-03-22 22:52:50 +0530

    integrated firestore db

--- src/utils/firebase/index.js
+++ src/utils/firebase/index.js
@@ -1,6 +1,9 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 
+import { getFirestore, collection, addDoc } from "firebase/firestore";
+
+
 const firebaseConfig = {
     apiKey: "AIzaSyAhVOXc5FiOZ6scvr-WiM7LV-eqUOp7BYg",
     authDomain: "crwn-clothing-cc3da.firebaseapp.com",
@@ -14,10 +17,44 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 const provider = new GoogleAuthProvider();
-
 const auth = getAuth(app);
 
+//firestore db
+const db = getFirestore(app);
 
-export const signInWithGooglePopUp = () => signInWithPopup(auth, provider);
+// export const createPerson = async () => {
+//     try {
+//         const docRef = await addDoc(collection(db, "persons"), {
+//             first: "Saksham",
+//             middle: "",
+//             last: "Gupta",
+//             born: 2000
+//         });
+
+//         console.log("Document written with ID: ", docRef.id);
+//     } catch (e) {
+//         console.error("Error adding document: ", e);
+//     }
+// };
+
+
+
+
+export const signInWithGooglePopUp = () => signInWithPopup(auth, provider).then(async (result) => {
+    try {
+        const docRef = await addDoc(collection(db, "persons"), {
+            first: "Saksham",
+            middle: "",
+            last: "Gupta",
+            born: 2000
+        });
+
+        console.log("Document written with ID: ", docRef.id);
+        console.log(result);
+    } catch (e) {
+        console.error("Error adding document: ", e);
+    }
+
+});
 
 
