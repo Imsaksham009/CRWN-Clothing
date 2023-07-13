2252717993821236bf491c94c352f9beee5ef599
Saksham Gupta <guptasaksham82@gmail.com>
2023-07-13 22:51:27 +0530

    Implemented Google Sign IN using Firebase

--- src/Routes/Auth/sign-in.component.jsx
+++ src/Routes/Auth/sign-in.component.jsx
@@ -1,7 +1,19 @@
+import {
+	googleSignInWithPopUp,
+	setUserFromAuth,
+} from "../../Utils/Firebase/firebase.utils";
+
 const SignIn = () => {
+	const signINWithGooglePopUp = async () => {
+		const result = await googleSignInWithPopUp();
+		console.log(result.user);
+		const saveResult = await setUserFromAuth(result.user);
+		console.log(saveResult);
+	};
 	return (
 		<div>
 			<h1>Sign IN Page</h1>
+			<button onClick={signINWithGooglePopUp}>Google Sign-IN</button>
 		</div>
 	);
 };
--- src/Utils/Firebase/firebase.utils.js
+++ src/Utils/Firebase/firebase.utils.js
@@ -1,5 +1,11 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import {
+    getAuth,
+    signInWithPopup,
+    GoogleAuthProvider
+} from "firebase/auth";
+import { getFirestore, doc, setDoc, getDoc } from "firebase/firestore";
+
 
 
 const firebaseConfig = {
@@ -10,6 +16,36 @@ const firebaseConfig = {
     messagingSenderId: "396640260522",
     appId: "1:396640260522:web:d06895efc06522f7011bd3"
 };
-// Initialize Firebase
+
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
+
+export const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+
+export const db = getFirestore(app);
+
+
+export const googleSignInWithPopUp = () => {
+    return signInWithPopup(auth, provider);
+};
+
+
+export const setUserFromAuth = async (authData) => {
+    const docRef = doc(db, "Users", authData.uid);
+    const userSnap = await getDoc(docRef);
+    try {
+        if (!userSnap.exists()) {
+            await setDoc(docRef, {
+                displayName: authData.displayName,
+                email: authData.email,
+                createdOn: Date().toString()
+            });
+        }
+    } catch (e) {
+        console.log(e);
+    }
+
+    return docRef;
+
+}
+
